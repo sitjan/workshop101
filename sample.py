4744d9a0c7911e64c11922e0c2960ccb8d640b0f
sitjan <sitaulajan@gmail.com>
2024-02-27 12:52:58 +0545

    Sample program

---
+++ sample.py
@@ -0,0 +1,8 @@
+# name = input("Enter your name: ")
+# S = "The name of the user is "
+# print(S + name)
+# #
+
+x = int(input("Enter a number: "))
+output = 5 * x
+print("The product of the number with 5 is", output)
